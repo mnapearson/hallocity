8d21574137bfa19f2000db473ef87ecaf374b822
mnapearson <marratoon@gmail.com>
2021-04-27 15:47:00 +0200

    adjust countdown and impressum

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var deadline = new Date("may 1, 2021, 9:00:00").getTime();
+var deadline = new Date("June 1, 2021, 9:00:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
   var t = deadline - now;
